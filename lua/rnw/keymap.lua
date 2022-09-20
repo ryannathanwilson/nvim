6e4a22901da87de119cebd6f1535b163c02ec039
Ryan N Wilson <ryan.nathan.wilson@gmail.com>
2022-09-19 20:11:10 -0400

    Git configuration

--- lua/rnw/keymap.lua
+++ lua/rnw/keymap.lua
@@ -35,26 +35,29 @@ map('n', '<leader>pwd', function() vim.cmd [[echo expand("#1:p")]] end)
 map('n', '<C-G>', function() vim.cmd [[NvimTreeFindFileToggle]] end)
 
 -- GIT
-local gitsigns = plugin_options.gitsigns
-local neogit = require 'neogit'
 
-map('n', '<leader>gs', function() neogit.open() end)
-map('n', '<leader>dv', function() vim.cmd [[DiffviewOpen]] end)
-map('n', '<leader>hn', gitsigns.next_hunk, { expr = true })
-map('n', '<leader>hp', gitsigns.prev_hunk, { expr = true })
+map('n', '<leader>gs', function() vim.cmd [[Git]] end)
 
-map({ 'n', 'v' }, '<leader>hs', gitsigns.loaded.stage_hunk)
-map({ 'n', 'v' }, '<leader>hr', gitsigns.loaded.reset_hunk)
-map('n', '<leader>hu', gitsigns.loaded.undo_stage_hunk)
+-- local gitsigns = plugin_options.gitsigns
+-- local neogit = require 'neogit'
 
-map('n', '<leader>hS', gitsigns.loaded.stage_buffer)
-map('n', '<leader>hR', gitsigns.loaded.reset_buffer)
-map('n', '<leader>hp', gitsigns.loaded.preview_hunk)
-map('n', '<leader>hb', function() gitsigns.loaded.blame_line { full = true } end)
-map('n', '<leader>tb', gitsigns.loaded.toggle_current_line_blame)
-map('n', '<leader>hd', gitsigns.loaded.diffthis)
-map('n', '<leader>hD', function() gitsigns.loaded.diffthis('~') end)
-map('n', '<leader>td', gitsigns.loaded.toggle_deleted)
+-- map('n', '<leader>gs', function() neogit.open() end)
+-- map('n', '<leader>dv', function() vim.cmd [[DiffviewOpen]] end)
+-- map('n', '<leader>hn', gitsigns.next_hunk, { expr = true })
+-- map('n', '<leader>hp', gitsigns.prev_hunk, { expr = true })
+
+-- map({ 'n', 'v' }, '<leader>hs', gitsigns.loaded.stage_hunk)
+-- map({ 'n', 'v' }, '<leader>hr', gitsigns.loaded.reset_hunk)
+-- map('n', '<leader>hu', gitsigns.loaded.undo_stage_hunk)
+--
+-- map('n', '<leader>hS', gitsigns.loaded.stage_buffer)
+-- map('n', '<leader>hR', gitsigns.loaded.reset_buffer)
+-- map('n', '<leader>hp', gitsigns.loaded.preview_hunk)
+-- map('n', '<leader>hb', function() gitsigns.loaded.blame_line { full = true } end)
+-- map('n', '<leader>tb', gitsigns.loaded.toggle_current_line_blame)
+-- map('n', '<leader>hd', gitsigns.loaded.diffthis)
+-- map('n', '<leader>hD', function() gitsigns.loaded.diffthis('~') end)
+-- map('n', '<leader>td', gitsigns.loaded.toggle_deleted)
 
 -- Search
 map('n', '<c-/>', function() vim.cmd [[Telescope current_buffer_fuzzy_find]] end)
--- lua/rnw/plugin_options.lua
+++ lua/rnw/plugin_options.lua
@@ -9,51 +9,54 @@ require("rnw.lang")
 -- ================================== --
 -- GIT --
 -- ================================== --
-require('neogit').setup({
-  kind = 'vsplit',
-  integrations = {
-      diffview = true
-    },
-})
-require('gitsigns').setup()
-local gs = package.loaded.gitsigns
-M.gitsigns = {
-  loaded = gs,
-  next_hunk = function()
-    vim.schedule(function() gs.next_hunk() end)
-    return '<Ignore>'
-  end,
-  prev_hunk = function()
-    vim.schedule(function() gs.prev_hunk() end)
-    return '<Ignore>'
-  end,
-}
+--
+--
 
-require('diffview').setup({
-  hooks = {
-    view_opened = function(view)
-      local DiffView = require("diffview.views.diff.diff_view").DiffView
-      if view:instanceof(DiffView) then
-        -- Refresh diffview when the git index changes:
-        local watcher = vim.loop.new_fs_poll()
-        watcher:start(view.git_dir .. "/index", 1000, function(err, prev, cur)
-          if not err then
-            vim.schedule(function()
-              vim.cmd("DiffviewRefresh")
-            end)
-          end
-        end)
-
-        -- Stop the watcher when the view closes:
-        DiffviewGlobal.emitter:on("view_closed", function(v)
-          if v == view then
-            watcher:stop()
-          end
-        end)
-      end
-    end,
-  },
-})
+-- require('neogit').setup({
+--   kind = 'vsplit',
+--   integrations = {
+--       diffview = true
+--     },
+-- })
+-- require('gitsigns').setup()
+-- local gs = package.loaded.gitsigns
+-- M.gitsigns = {
+--   loaded = gs,
+--   next_hunk = function()
+--     vim.schedule(function() gs.next_hunk() end)
+--     return '<Ignore>'
+--   end,
+--   prev_hunk = function()
+--     vim.schedule(function() gs.prev_hunk() end)
+--     return '<Ignore>'
+--   end,
+-- }
+--
+-- require('diffview').setup({
+--   hooks = {
+--     view_opened = function(view)
+--       local DiffView = require("diffview.views.diff.diff_view").DiffView
+--       if view:instanceof(DiffView) then
+--         -- Refresh diffview when the git index changes:
+--         local watcher = vim.loop.new_fs_poll()
+--         watcher:start(view.git_dir .. "/index", 1000, function(err, prev, cur)
+--           if not err then
+--             vim.schedule(function()
+--               vim.cmd("DiffviewRefresh")
+--             end)
+--           end
+--         end)
+--
+--         -- Stop the watcher when the view closes:
+--         DiffviewGlobal.emitter:on("view_closed", function(v)
+--           if v == view then
+--             watcher:stop()
+--           end
+--         end)
+--       end
+--     end,
+--   },
+-- })
 
 -- ================================== --
 -- NVIM Comment --
@@ -94,7 +97,7 @@ require("nvim-tree").setup({
       },
     },
   },
-  open_on_setup = true,
+  open_on_setup = false,
   renderer = {
     group_empty = true,
     icons = {
